820f01a9a21bbc466de06f6999b1aa041712e02b
TimLai666 <tim930102@icloud.com>
2025-05-21 23:57:52 +0800

    新增OLLAMA_BASE_URL_DEV環境變數，更新config.go以支援開發模式，調整docker-compose.yml的網路配置

--- bot/config/config.go
+++ bot/config/config.go
@@ -7,6 +7,8 @@ import (
 	"github.com/joho/godotenv"
 )
 
+const devMode bool = false
+
 var (
 	OLLAMA_BASE_URL     string
 	LINE_CHANNEL_TOKEN  string
@@ -18,8 +20,11 @@ func init() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
-	OLLAMA_BASE_URL = os.Getenv("OLLAMA_BASE_URL")
+	if devMode {
+		OLLAMA_BASE_URL = os.Getenv("OLLAMA_BASE_URL_DEV")
+	} else {
+		OLLAMA_BASE_URL = os.Getenv("OLLAMA_BASE_URL")
+	}
 	LINE_CHANNEL_TOKEN = os.Getenv("LINE_CHANNEL_TOKEN")
 	LINE_CHANNEL_SECRET = os.Getenv("LINE_CHANNEL_SECRET")
 }
